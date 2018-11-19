d66a0d2717dc2e92bd00c8006f44e5fa13bd763f
James Chuang <chuang.ja@gmail.com>
2018-11-19 18:31:17 -0500

    Update dependencies, slurm scripts, cluster requests.

--- slurm.sh
+++ slurm.sh
@@ -2,10 +2,10 @@
 
 #SBATCH -p medium
 #SBATCH -t 24:00:00
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=1G
 #SBATCH -n 1
 #SBATCH -e snakemake.err
 #SBATCH -o snakemake.log
 #SBATCH -J motifs-snakemake
 
-snakemake -p -R `cat <(snakemake --lc) <(snakemake --li) <(snakemake --lp)` --latency-wait 300 --rerun-incomplete --cluster-config cluster.yaml --use-conda --jobs 999 --restart-times 1 --cluster "sbatch -p {cluster.queue} -n {cluster.n} -t {cluster.time} --mem-per-cpu={cluster.mem} -J {cluster.name} -e {cluster.err} -o {cluster.log}"
+snakemake -p -R `cat <(snakemake --lc --rerun-incomplete) <(snakemake --li --rerun-incomplete) <(snakemake --lp --rerun-incomplete) | sort -u` --latency-wait 300 --rerun-incomplete --cluster-config cluster.yaml --use-conda --jobs 9999 --restart-times 1 --cluster "sbatch -p {cluster.queue} -n {cluster.n} -t {cluster.time} --mem-per-cpu={cluster.mem} -J {cluster.name} -e {cluster.err} -o {cluster.log}"
